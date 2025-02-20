843457df7e34846fb64070d83d92284830e0410d
dn0ne <mmmdanone04@gmail.com>
2025-02-20 09:59:25 +0200

    add HttpClient to module

--- app/src/main/java/com/dn0ne/banking/di/AppModule.kt
+++ app/src/main/java/com/dn0ne/banking/di/AppModule.kt
@@ -1,7 +1,17 @@
 package com.dn0ne.banking.di
 
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
 import org.koin.dsl.module
 
 val appModule = module {
-
+    single<HttpClient> {
+        HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json()
+            }
+        }
+    }
 }
