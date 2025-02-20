4578c4bba1dde89282b86a87b44a7af8506f56eb
dn0ne <mmmdanone04@gmail.com>
2025-02-20 15:50:45 +0200

    add api config

---
+++ app/src/main/java/com/dn0ne/banking/data/ApiConfig.kt
@@ -0,0 +1,9 @@
+package com.dn0ne.banking.data
+
+object ApiConfig {
+    private const val BASE_URL = "http://localhost:8080/api"
+    const val REGISTER_ENDPOINT = "$BASE_URL/register"
+    const val LOGIN_ENDPOINT = "$BASE_URL/login"
+    const val ACCOUNT_ENDPOINT = "$BASE_URL/account"
+    const val TRANSACTION_ENDPOINT = "$BASE_URL/transaction"
+}
