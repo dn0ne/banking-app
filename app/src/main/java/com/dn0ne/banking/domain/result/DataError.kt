03c3d7e93e852280734a2c0b997fed2aa807c351
dn0ne <mmmdanone04@gmail.com>
2025-02-20 15:49:59 +0200

    add result success-error utility classes

---
+++ app/src/main/java/com/dn0ne/banking/domain/result/DataError.kt
@@ -0,0 +1,21 @@
+package com.dn0ne.banking.domain.result
+
+sealed interface DataError : Error {
+    enum class Network : DataError {
+        Conflict,
+        VerificationRequired,
+        LoginFailed,
+        Forbidden,
+        Unknown,
+
+        InternalServerError
+    }
+
+    enum class Transaction : DataError {
+        BadTransaction,
+        InsufficientFunds,
+        AccountNotFound,
+        AccountIsClosed,
+        InternalError
+    }
+}
---
+++ app/src/main/java/com/dn0ne/banking/domain/result/Error.kt
@@ -0,0 +1,3 @@
+package com.dn0ne.banking.domain.result
+
+sealed interface Error
