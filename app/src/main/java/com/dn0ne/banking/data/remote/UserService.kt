3ef757a9b2874f021efed0393a9d044163d3e088
dn0ne <mmmdanone04@gmail.com>
2025-02-20 15:51:03 +0200

    add user service

---
+++ app/src/main/java/com/dn0ne/banking/data/remote/UserService.kt
@@ -0,0 +1,54 @@
+package com.dn0ne.banking.data.remote
+
+import com.dn0ne.banking.data.ApiConfig
+import com.dn0ne.banking.domain.User
+import com.dn0ne.banking.domain.result.DataError
+import com.dn0ne.banking.domain.result.Result
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.http.HttpStatusCode
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.Serializable
+
+class UserService(
+    private val client: HttpClient
+) {
+
+    suspend fun register(user: User): Result<Unit, DataError.Network> =
+        withContext(Dispatchers.IO) {
+            val response = client.post(ApiConfig.REGISTER_ENDPOINT) {
+                setBody(user)
+            }
+
+            when (response.status) {
+                HttpStatusCode.Created -> Result.Success(Unit)
+                HttpStatusCode.Unauthorized -> Result.Error(DataError.Network.VerificationRequired)
+                HttpStatusCode.Conflict -> Result.Error(DataError.Network.Conflict)
+                else -> Result.Error(DataError.Network.Unknown)
+            }
+        }
+
+    suspend fun login(user: User): Result<String, DataError.Network> =
+        withContext(Dispatchers.IO) {
+            val response = client.post(ApiConfig.LOGIN_ENDPOINT) {
+                setBody(user)
+            }
+
+            when (response.status) {
+                HttpStatusCode.OK -> {
+                    val token = response.body<TokenDto>()
+                    Result.Success(token.token)
+                }
+                HttpStatusCode.Unauthorized -> Result.Error(DataError.Network.LoginFailed)
+                else -> Result.Error(DataError.Network.Unknown)
+            }
+        }
+}
+
+@Serializable
+private data class TokenDto(
+    val token: String
+)
