81015596817ec202874316ae4af9a28347395c81
dn0ne <mmmdanone04@gmail.com>
2025-02-23 11:47:21 +0200

    refactor: fix api url

--- app/src/main/java/com/dn0ne/banking/data/ApiConfig.kt
+++ app/src/main/java/com/dn0ne/banking/data/ApiConfig.kt
@@ -1,7 +1,7 @@
 package com.dn0ne.banking.data
 
 object ApiConfig {
-    private const val BASE_URL = "http://localhost:8080/api"
+    private const val BASE_URL = "http://10.0.2.2:8080/api"
     const val REGISTER_ENDPOINT = "$BASE_URL/register"
     const val LOGIN_ENDPOINT = "$BASE_URL/login"
     const val ACCOUNT_ENDPOINT = "$BASE_URL/account"
