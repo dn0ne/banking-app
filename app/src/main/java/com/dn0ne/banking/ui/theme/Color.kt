82be22dddef12f92ff0a50c5b7285fdecb8d7613
dn0ne <mmmdanone04@gmail.com>
2025-02-23 11:48:13 +0200

    add colors

--- app/src/main/java/com/dn0ne/banking/ui/theme/Color.kt
+++ app/src/main/java/com/dn0ne/banking/ui/theme/Color.kt
@@ -3,4 +3,8 @@ package com.dn0ne.banking.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Blue = Color(0xFFA497F9)
+val BlueDarker = Color(0xFF7D70D5)
 val BlueDark = Color(0xFF5F5791)
+
+val DarkBlue = Color(0xFF213048)
+val DarkPurple = Color(0xFF432148)
