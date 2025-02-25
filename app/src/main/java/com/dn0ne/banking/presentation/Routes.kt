c3da1845f705b02d4d70fc4c229e1997ad95d1e5
dn0ne <mmmdanone04@gmail.com>
2025-02-25 18:51:21 +0200

    add navigation routes

---
+++ app/src/main/java/com/dn0ne/banking/presentation/Routes.kt
@@ -0,0 +1,26 @@
+package com.dn0ne.banking.presentation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+sealed interface Routes {
+    @Serializable
+    data object Authentication: Routes {
+        @Serializable
+        data object Welcome
+        @Serializable
+        data object Login
+        @Serializable
+        data object Signup
+        @Serializable
+        data object Verification
+    }
+
+    @Serializable
+    data object Main: Routes {
+        @Serializable
+        data object Home
+        @Serializable
+        data object Transfers
+    }
+}
