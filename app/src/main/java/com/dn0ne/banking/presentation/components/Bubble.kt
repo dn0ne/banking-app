fd94bac8a8a175f21e70db53b1154ab6aad679c4
dn0ne <mmmdanone04@gmail.com>
2025-02-25 18:45:16 +0200

    move Bubble to a separate composable

---
+++ app/src/main/java/com/dn0ne/banking/presentation/components/Bubble.kt
@@ -0,0 +1,44 @@
+package com.dn0ne.banking.presentation.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.dn0ne.banking.R
+
+@Composable
+fun Bubble(
+    modifier: Modifier = Modifier,
+    imageOffset: Int = 0
+) {
+    Box(
+        modifier = modifier
+            .aspectRatio(1f)
+            .shadow(
+                8.dp,
+                shape = CircleShape,
+            )
+            .clip(CircleShape)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.gradient),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alignment = when (imageOffset % 3) {
+                0 -> Alignment.Center
+                1 -> Alignment.CenterStart
+                else -> Alignment.CenterEnd
+            },
+            modifier = Modifier
+                .matchParentSize()
+        )
+    }
+}
