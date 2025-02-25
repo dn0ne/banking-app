c4acb29115936f8028797c6095c900fb599686a7
dn0ne <mmmdanone04@gmail.com>
2025-02-25 18:47:12 +0200

    add auth view model to module

--- app/src/main/java/com/dn0ne/banking/di/AppModule.kt
+++ app/src/main/java/com/dn0ne/banking/di/AppModule.kt
@@ -3,11 +3,13 @@ package com.dn0ne.banking.di
 import com.dn0ne.banking.data.remote.UserService
 import com.dn0ne.banking.data.remote.AccountService
 import com.dn0ne.banking.data.remote.TransactionService
+import com.dn0ne.banking.presentation.authentication.AuthenticationViewModel
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
 import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val appModule = module {
@@ -22,4 +24,6 @@ val appModule = module {
     singleOf(::UserService)
     singleOf(::AccountService)
     singleOf(::TransactionService)
+
+    viewModelOf(::AuthenticationViewModel)
 }
